649c197d8d134b3b2be3b33e387fb0b277b5a6c4
Declan Smith <dwsmith7@ncsu.edu>
2022-11-02 11:05:08 -0400

    Added new directory for hw4
    Copied hw3 into new directory for hw4

--- hw2/part3/client.cpp
+++ hw2/part3/client.cpp
@@ -9,11 +9,11 @@ int main(void) {
 
     //connect to the server with a req socket
     zmq::context_t context(1);
-    const char * reqProtocol = "tcp://127.0.0.1:5555";
+    const char * repProtocol = "tcp://127.0.0.1:5555";
     const char * subProtocol = "tcp://127.0.0.1:5556";
-    zmq::socket_t mySocket (context, zmq::socket_type::req);
+    zmq::socket_t mySocket (context, zmq::socket_type::rep);
     zmq::socket_t subSocket (context, zmq::socket_type::sub);
-    mySocket.connect(reqProtocol);
+    mySocket.connect(repProtocol);
     subSocket.connect(subProtocol);
 
     subSocket.setsockopt(ZMQ_SUBSCRIBE, "", 0);
@@ -23,29 +23,67 @@ int main(void) {
       return 1;
     }
 
-    //do intitial req
-    json initReq = {
+    //create client state
+    json client = {
         {"id", -1},
         {"iteration", 0},
     };
 
-    std::string msgStr = initReq.dump();
-    zmq::message_t msg(msgStr.c_str(), msgStr.length());
-    mySocket.send(msg, zmq::send_flags::none);
+    //go ahead and make shutdown message:
+    
+    for(int i = 0; i < 20; i++) {
+        // std::cout << "wating for server" << std::endl;
+        zmq::message_t reply;
+        mySocket.recv(reply, zmq::recv_flags::none);
 
-    //dont need to do anything with this reply
-    zmq::message_t reply;
-    mySocket.recv(reply, zmq::recv_flags::none);
-    mySocket.close();
+        json update = json::parse(reply.to_string());
 
-    //now just print out every message published to subscriber socket
-    while(true) {
-        zmq::message_t data;
-        subSocket.recv(data, zmq::recv_flags::none);
-        json client = json::parse(data.to_string());
-        std::cout << "Client " << client["id"] << ": Iteration " << client["iteration"] << std::endl;
+        // std::cout << update.dump().c_str() << std::endl;
+
+        client["iteration"] = i;
+
+        int iteration = update["iteration"];
+
+        if(client["id"] == -1) {
+            client["id"] = update["nextId"];
+        }
+
+
+        std::string msgStr = client.dump();
+        zmq::message_t msg(msgStr.c_str(), msgStr.length());
+        mySocket.send(msg, zmq::send_flags::none);
+
+        //dont need to do anything with this reply
+        // mySocket.close();
+
+        //now just print out every message published to subscriber socket
+        bool reading = true;
+        while(reading) {
+            // std::cout << "reading" << std::endl;
+            zmq::message_t data;
+            subSocket.recv(data, zmq::recv_flags::none);
+            // std::cout << "parsing: " << data.to_string() << std::endl;
+            if(data.to_string().length() > 0) {
+                json update = json::parse(data.to_string());
+                if(update["id"] == 0) {
+                    // std::cout << "no longer reading" << std::endl;
+                    std::cout << update["iteration"] << std::endl;
+                    if(update["iteration"] == iteration) {
+                        reading = false;
+                    }
+                } else {
+                    std::cout << "Client " << update["id"] << ": Iteration " << update["iteration"] << std::endl;
+                }
+            }
+        }
     }
+    zmq::message_t foo;
+    mySocket.recv(foo, zmq::recv_flags::none);
+    client["iteration"] = -1;
+    zmq::message_t shutdown(client.dump().c_str(), client.dump().length());
+    mySocket.send(shutdown, zmq::send_flags::none);
 
+    mySocket.close();
     subSocket.close();
     context.close();    
 
--- hw2/part3/server.cpp
+++ hw2/part3/server.cpp
@@ -1,6 +1,6 @@
 #include <zmq.hpp>
 #include <string>
-#include<unistd.h>
+#include <unistd.h>
 #include <iostream>
 #include <nlohmann/json.hpp>
 
@@ -8,51 +8,71 @@ using json = nlohmann::json;
 
 int main(void) {
 
-    json serverData;
-
-    std::vector<json> clients;
+    // std::vector<json> clients;
     int clientCount = 0;
+    int nextId = 1;
 
     //create and connect to the socket
     zmq::context_t context(1);
-    zmq::socket_t mySocket (context, zmq::socket_type::rep);
+    zmq::socket_t mySocket (context, zmq::socket_type::req);
     zmq::socket_t pubSocket (context, zmq::socket_type::pub);
     // const char * protocol = "ipc:///tmp/test";
-    const char * repProtocol = "tcp://127.0.0.1:5555";
+    const char * reqProtocol = "tcp://127.0.0.1:5555";
     const char * pubProtocol = "tcp://127.0.0.1:5556";
 
-    mySocket.bind(repProtocol);
+    mySocket.bind(reqProtocol);
     pubSocket.bind(pubProtocol);
 
-    if(!pubSocket.connected()) {
+    if(!pubSocket.connected() || !mySocket.connected()) {
       std::cout << "socket cannot be bound??" << std::endl;
       return 1;
     }
     
     int iteration = 0;
 
-      json j = {
-        {"iteration", 0},
-      };
+    //create a sentinel message to publish at the end of each loop
+    json end = {
+      {"id", -1},
+      {"server iteration", 0},
+    };
+
+
+    json serverData = {
+      {"id", 0},
+      {"nextId", 1},
+      {"iteration", 0},
+    };
+
+
+
     while(true) {
-      zmq::message_t req;
+      serverData["iteration"] = iteration++;
+      int i = 0;
+      do{
+        // std::cout << "sending" << std::endl;
+        mySocket.send(zmq::message_t(serverData.dump().c_str(), serverData.dump().length()), zmq::send_flags::none);
+        // std::cout << "sent" << std::endl;
 
-      zmq::recv_result_t status = mySocket.recv(req, zmq::recv_flags::dontwait);
-      if(status.has_value()) {
-        clients.push_back(json::parse(req.to_string()));
-        clients[clients.size() - 1]["id"] = clients.size();
-        mySocket.send(zmq::message_t(clients[clients.size() -1].dump().c_str(), clients[clients.size() -1].dump().length()), zmq::send_flags::none);
-      }
-      for(int i = 0; i < clients.size(); i++) {
-        int iteration = clients.at(i)["iteration"];
-        clients.at(i)["iteration"] = iteration + 1;
-      }
+        zmq::message_t req;
+        zmq::recv_result_t status = mySocket.recv(req, zmq::recv_flags::none);
+        json response = json::parse(req.to_string());
+        std::cout << response.dump().c_str() << std::endl;
+        
+        if(response["id"] == serverData["nextId"]) {
+          clientCount++;
+          nextId++;
+          serverData["nextId"] = nextId;
+        } 
+        if(response["iteration"] == -1) {
+          clientCount--;
+        } else {
+          pubSocket.send(req, zmq::send_flags::none);
+        }
 
-      for(json client : clients) {
-        zmq::message_t data(client.dump().c_str(), client.dump().length());
-        pubSocket.send(data, zmq::send_flags::none);
-      }
+      } while(++i < clientCount);
       sleep(1);
+      zmq::message_t sentinel(serverData.dump().c_str(), serverData.dump().length());
+      pubSocket.send(sentinel, zmq::send_flags::none);
       }
 
     //close whats gotta close and exit
